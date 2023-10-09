89de4c31d3a2dbc301bf2aeb24cdb9db36c40faf
13010-34-10 <13010-34-10@exchange.ad.afpanet>
2023-10-09 16:10:01 +0200

    fin-exo

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+let maTable = document.createElement("table");
+document.body.appendChild(maTable);
+for (let nbRows = 1; nbRows <= 10; nbRows++) {
+    let uneRow = document.createElement("tr");
+    maTable.appendChild(uneRow);
+    for (let nbcells = 1; nbcells <= 10; nbcells++) {
+        let uneCellule = document.createElement("td");
+        uneCellule.textContent = (nbRows*nbcells)
+        uneRow.appendChild(uneCellule);
+    }
+}
